302b87ad0cae87106d6941f040f25f404ca3fa12
blitzkz23 <blitzaldykazu@gmail.com>
2023-01-01 08:33:10 +0700

    add command instruction

--- dockerfile/script.sh
+++ dockerfile/script.sh
@@ -4,5 +4,13 @@ docker image ls
 
 # RUN Instruction 
 docker build -t mznopal/run run
-## Untuk run yang lebih detail
+## Untuk run yang lebih detail pada log
 docker build -t mznopal/run run --progress=plain --no-cache
+
+# COMMAND Instruction
+docker build -t mznopal/command command
+docker image inspect mznopal/command # untuk lihat bash command yang dijalankan
+## Buat container dari image command
+docker container create --name command mznopal/command
+docker container start command
+docker container logs command
